8bafb9d1d8a3fdfbf1972d51b2a0f1585c84e755
amaankhanakz <sameeramaan2014@gmail.com>
2023-04-20 22:07:54 +0530

    search in a rotated sorted array

---
+++ Searching Sorting/searchinrotatedsortedarray.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int searchInRotatedArray(int arr[], int key, int left, int right)
+{
+  if (left > right)
+  {
+    return -1;
+  }
+
+  int mid = (left + right) / 2;
+
+  if (arr[mid] == key)
+  {
+    return mid;
+  }
+  if (arr[left] <= arr[mid])
+  {
+    if (key >= arr[left] && key <= arr[mid])
+    {
+      return searchInRotatedArray(arr, key, left, mid - 1);
+    }
+    return searchInRotatedArray(arr, key, mid + 1, right);
+  }
+  if (key >= arr[mid] && key <= arr[right])
+  {
+    return searchInRotatedArray(arr, key, mid + 1, right);
+  }
+  return searchInRotatedArray(arr, key, left, mid - 1);
+}
+
+int main()
+{
+
+  int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
+  int n = 8;
+  int key = 30;
+
+  int idx = searchInRotatedArray(arr, key, 0, n - 1);
+
+  if (idx == -1)
+  {
+    cout << "Not present";
+  }
+  else
+  {
+    cout << idx;
+  }
+
+  return 0;
+}
